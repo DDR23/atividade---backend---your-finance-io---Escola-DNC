dc15a2c819504aa4e08e13da8b1abfe6f96716aa
DDR23 <roboreserva.andre@gmail.com>
2024-05-08 22:07:24 -0300

    feat:{add dotenv configs}

---
+++ src/db/config.js
@@ -0,0 +1,8 @@
+//ATRIBUI VALOR AS VARIÁVEIS DE ACORDO COM A EXISTENCIA DAS VARIAVEIS DE AMBIENTE
+module.exports = {
+  DB_USER: process.env.DB_USER || process.env.DB_LOCAL_USER,
+  DB_PASSWORD: process.env.DB_PASSWORD || process.env.DB_LOCAL_PASSWORD,
+  DB_HOST: process.env.DB_HOST || process.env.DB_LOCAL_HOST,
+  DB_PORT: process.env.DB_PORT || process.env.DB_LOCAL_PORT,
+  DB_SCHEMA: process.env.DB_SCHEMA || process.env.DB_LOCAL_SCHEMA,
+};
