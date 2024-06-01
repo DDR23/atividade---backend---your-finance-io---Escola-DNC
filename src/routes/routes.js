646a792fd37df1e5495d46b205991f48a4c76c77
DDR23 <roboreserva.andre@gmail.com>
2024-06-01 14:03:36 -0300

    feat:{add authenticateToken middleware}

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,14 +2,15 @@
 function routes(app) {
   //HOME
   app.use('/', require('./index'));
+  app.use('/', require('./user/userMe'));
 
   //USER
   app.use('/user', require('./user/postUser'));
-  app.use('/user', require('./user/loginUser'));
   app.use('/user', require('./user/getUser'));
   app.use('/user', require('./user/getOneUser'));
   app.use('/user', require('./user/putUser'));
   app.use('/user', require('./user/deleteUser'));
+  app.use('/user', require('./user/loginUser'));
 
   //CATEGORY
   app.use('/category', require('./category/postCategory'));
---
+++ src/routes/user/userMe.js
@@ -0,0 +1,36 @@
+//CONFIG. PADRÃO DE ROTEAMENTO E IMPORTAÇÕES
+const express = require('express');
+const router = express.Router();
+const schemaUser = require('../../schemas/schemaUser');
+const authenticateToken = require('../../middlewares/authenticateToken');
+
+//REQUISIÇÃO HTTP
+router.get('/me', authenticateToken, async (req, res) => {
+
+  //EXECUTA TODO ESSE BLOCO AO BATER NA ROTA
+  try {
+
+    //VERIFICA SE O USUARIO EXISTE
+    const user = await schemaUser.findByPk(req.user.id);
+    if (!user) {
+      return res.status(404).json({
+        error: 'User not found',
+        message: `That user you're looking for doesn't exist in the database.`,
+        code: 404
+      });
+    }
+
+    //RETORNA O RESULTADO
+    return res.status(200).json(user);
+
+  //RETORNA ERRO CASO A EXECUÇÃO ACIMA FALHE
+  } catch (error) {
+    return res.status(500).json({
+      error: 'Internal server error',
+      message: 'Unable to obtain this user due to an internal server error. Please try again later.',
+      code: 500
+    });
+  }
+});
+
+module.exports = router;
